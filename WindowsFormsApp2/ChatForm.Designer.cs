9d8d6e5c5d34c15bd17e4c3064f58163b53ccb34
Cranot <44682693+Cranot@users.noreply.github.com>
2023-03-05 19:50:48 +0200

    v05 out

--- WindowsFormsApp2/ChatForm.Designer.cs
+++ WindowsFormsApp2/ChatForm.Designer.cs
@@ -31,7 +31,7 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleChatGPT));
 			this.label1 = new System.Windows.Forms.Label();
 			this.apibox = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
+			this.send_button = new System.Windows.Forms.Button();
 			this.resultbox = new System.Windows.Forms.TextBox();
 			this.promptbox = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
@@ -48,6 +48,20 @@
 			this.button4 = new System.Windows.Forms.Button();
 			this.label5 = new System.Windows.Forms.Label();
 			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.total_spent_box = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.completion_token_box = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.prompt_token_box = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.total_token_box = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.total_m_spent_box = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.current_spent_box = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -68,22 +82,22 @@
 			this.apibox.Text = "YOUR API HERE";
 			this.apibox.TextChanged += new System.EventHandler(this.apibox_TextChanged);
 			// 
-			// button1
+			// send_button
 			// 
-			this.button1.Location = new System.Drawing.Point(606, 544);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(90, 30);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "Generate";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.send_button.Location = new System.Drawing.Point(606, 544);
+			this.send_button.Name = "send_button";
+			this.send_button.Size = new System.Drawing.Size(90, 30);
+			this.send_button.TabIndex = 2;
+			this.send_button.Text = "Send Prompt";
+			this.send_button.UseVisualStyleBackColor = true;
+			this.send_button.Click += new System.EventHandler(this.send_prompt_click);
 			// 
 			// resultbox
 			// 
-			this.resultbox.Location = new System.Drawing.Point(59, 234);
+			this.resultbox.Location = new System.Drawing.Point(59, 306);
 			this.resultbox.Multiline = true;
 			this.resultbox.Name = "resultbox";
-			this.resultbox.Size = new System.Drawing.Size(637, 304);
+			this.resultbox.Size = new System.Drawing.Size(637, 232);
 			this.resultbox.TabIndex = 3;
 			// 
 			// promptbox
@@ -92,7 +106,7 @@
 			this.promptbox.Multiline = true;
 			this.promptbox.Name = "promptbox";
 			this.promptbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.promptbox.Size = new System.Drawing.Size(637, 189);
+			this.promptbox.Size = new System.Drawing.Size(637, 261);
 			this.promptbox.TabIndex = 5;
 			this.promptbox.Text = "Paste or write here";
 			// 
@@ -108,16 +122,16 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(9, 571);
+			this.label3.Location = new System.Drawing.Point(29, 651);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(29, 13);
 			this.label3.TabIndex = 6;
-			this.label3.Text = "V0.4";
+			this.label3.Text = "V0.5";
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(44, 571);
+			this.label4.Location = new System.Drawing.Point(64, 651);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(56, 13);
 			this.label4.TabIndex = 7;
@@ -126,7 +140,7 @@
 			// linkLabel1
 			// 
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(101, 571);
+			this.linkLabel1.Location = new System.Drawing.Point(126, 651);
 			this.linkLabel1.Name = "linkLabel1";
 			this.linkLabel1.Size = new System.Drawing.Size(38, 13);
 			this.linkLabel1.TabIndex = 8;
@@ -147,7 +161,7 @@
 			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(304, 604);
+			this.button3.Location = new System.Drawing.Point(329, 697);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(92, 21);
 			this.button3.TabIndex = 11;
@@ -157,7 +171,7 @@
 			// 
 			// fontbox
 			// 
-			this.fontbox.Location = new System.Drawing.Point(252, 605);
+			this.fontbox.Location = new System.Drawing.Point(277, 697);
 			this.fontbox.Name = "fontbox";
 			this.fontbox.Size = new System.Drawing.Size(46, 20);
 			this.fontbox.TabIndex = 12;
@@ -167,16 +181,16 @@
 			this.mysterylistbox.FormattingEnabled = true;
 			this.mysterylistbox.Location = new System.Drawing.Point(702, 68);
 			this.mysterylistbox.Name = "mysterylistbox";
-			this.mysterylistbox.Size = new System.Drawing.Size(564, 160);
+			this.mysterylistbox.Size = new System.Drawing.Size(564, 264);
 			this.mysterylistbox.TabIndex = 13;
 			// 
 			// screenbox
 			// 
-			this.screenbox.Location = new System.Drawing.Point(702, 234);
+			this.screenbox.Location = new System.Drawing.Point(702, 338);
 			this.screenbox.Multiline = true;
 			this.screenbox.Name = "screenbox";
 			this.screenbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.screenbox.Size = new System.Drawing.Size(564, 304);
+			this.screenbox.Size = new System.Drawing.Size(564, 200);
 			this.screenbox.TabIndex = 14;
 			// 
 			// runbutton
@@ -193,14 +207,14 @@
 			// 
 			// familybox
 			// 
-			this.familybox.Location = new System.Drawing.Point(7, 605);
+			this.familybox.Location = new System.Drawing.Point(32, 696);
 			this.familybox.Name = "familybox";
 			this.familybox.Size = new System.Drawing.Size(138, 20);
 			this.familybox.TabIndex = 17;
 			// 
 			// button4
 			// 
-			this.button4.Location = new System.Drawing.Point(151, 604);
+			this.button4.Location = new System.Drawing.Point(176, 696);
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(95, 21);
 			this.button4.TabIndex = 16;
@@ -211,7 +225,7 @@
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(9, 584);
+			this.label5.Location = new System.Drawing.Point(29, 673);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(147, 13);
 			this.label5.TabIndex = 18;
@@ -220,7 +234,7 @@
 			// linkLabel2
 			// 
 			this.linkLabel2.AutoSize = true;
-			this.linkLabel2.Location = new System.Drawing.Point(156, 584);
+			this.linkLabel2.Location = new System.Drawing.Point(176, 673);
 			this.linkLabel2.Name = "linkLabel2";
 			this.linkLabel2.Size = new System.Drawing.Size(84, 13);
 			this.linkLabel2.TabIndex = 19;
@@ -229,13 +243,158 @@
 			this.linkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
 			// 
+			// total_spent_box
+			// 
+			this.total_spent_box.Location = new System.Drawing.Point(818, 551);
+			this.total_spent_box.Name = "total_spent_box";
+			this.total_spent_box.Size = new System.Drawing.Size(110, 20);
+			this.total_spent_box.TabIndex = 20;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(700, 554);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(101, 13);
+			this.label6.TabIndex = 21;
+			this.label6.Text = "Total Tokens Spent";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(699, 632);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(98, 13);
+			this.label7.TabIndex = 23;
+			this.label7.Text = "Completion Tokens";
+			// 
+			// completion_token_box
+			// 
+			this.completion_token_box.Location = new System.Drawing.Point(818, 629);
+			this.completion_token_box.Name = "completion_token_box";
+			this.completion_token_box.Size = new System.Drawing.Size(110, 20);
+			this.completion_token_box.TabIndex = 22;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(700, 606);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(79, 13);
+			this.label8.TabIndex = 25;
+			this.label8.Text = "Prompt Tokens";
+			// 
+			// prompt_token_box
+			// 
+			this.prompt_token_box.Location = new System.Drawing.Point(818, 603);
+			this.prompt_token_box.Name = "prompt_token_box";
+			this.prompt_token_box.Size = new System.Drawing.Size(110, 20);
+			this.prompt_token_box.TabIndex = 24;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(699, 658);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(107, 13);
+			this.label9.TabIndex = 27;
+			this.label9.Text = "Current Total Tokens";
+			// 
+			// total_token_box
+			// 
+			this.total_token_box.Location = new System.Drawing.Point(818, 655);
+			this.total_token_box.Name = "total_token_box";
+			this.total_token_box.Size = new System.Drawing.Size(110, 20);
+			this.total_token_box.TabIndex = 26;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(699, 580);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(71, 13);
+			this.label10.TabIndex = 29;
+			this.label10.Text = "Total $ Spent";
+			// 
+			// total_m_spent_box
+			// 
+			this.total_m_spent_box.Location = new System.Drawing.Point(818, 577);
+			this.total_m_spent_box.Name = "total_m_spent_box";
+			this.total_m_spent_box.Size = new System.Drawing.Size(110, 20);
+			this.total_m_spent_box.TabIndex = 30;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(700, 684);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(81, 13);
+			this.label11.TabIndex = 31;
+			this.label11.Text = "Current $ Spent";
+			// 
+			// current_spent_box
+			// 
+			this.current_spent_box.Location = new System.Drawing.Point(818, 681);
+			this.current_spent_box.Name = "current_spent_box";
+			this.current_spent_box.Size = new System.Drawing.Size(110, 20);
+			this.current_spent_box.TabIndex = 32;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Summarize",
+            "Find Bugs",
+            "Explain",
+            "Analyze",
+            "Find Key Points",
+            "Paraphrase",
+            "Evaluate",
+            "Compare and Contrast",
+            "Predict",
+            "Connect",
+            "Critique",
+            "Interpret",
+            "Categorize",
+            "Question",
+            "Visualize"});
+			this.comboBox1.Location = new System.Drawing.Point(59, 551);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 33;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(186, 551);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(85, 23);
+			this.button1.TabIndex = 34;
+			this.button1.Text = "Run This";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
 			// SimpleChatGPT
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoScroll = true;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.ClientSize = new System.Drawing.Size(1323, 672);
+			this.ClientSize = new System.Drawing.Size(1323, 740);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.current_spent_box);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.total_m_spent_box);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.total_token_box);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.prompt_token_box);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.completion_token_box);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.total_spent_box);
 			this.Controls.Add(this.linkLabel2);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.familybox);
@@ -252,7 +411,7 @@
 			this.Controls.Add(this.promptbox);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.resultbox);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.send_button);
 			this.Controls.Add(this.apibox);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -271,7 +430,7 @@
 
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox apibox;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button send_button;
 		private System.Windows.Forms.TextBox resultbox;
 		private System.Windows.Forms.TextBox promptbox;
 		private System.Windows.Forms.Label label2;
@@ -288,6 +447,20 @@
 		private System.Windows.Forms.Button button4;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.TextBox total_spent_box;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox completion_token_box;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox prompt_token_box;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox total_token_box;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox total_m_spent_box;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox current_spent_box;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- WindowsFormsApp2/ChatForm.cs
+++ WindowsFormsApp2/ChatForm.cs
@@ -11,6 +11,7 @@ namespace SimpleChatAPI
 {
 	public partial class SimpleChatGPT : Form
 	{
+		private string apiKey;
 
 		public SimpleChatGPT()
 		{
@@ -18,12 +19,15 @@ namespace SimpleChatAPI
 		}
 
 		bool checkInputs()
-		{
-			// some checks here
+		{	
 			if(string.IsNullOrEmpty(apibox.Text) || string.IsNullOrWhiteSpace(apibox.Text) || apibox.Text.Length < 2 || apibox.Text.Equals("YOUR API HERE"))
 			{
 				MessageBox.Show("Please enter a valid API key");
 				return false;
+			}
+			else
+			{
+				apiKey = apibox.Text;
 			}
 			if(string.IsNullOrEmpty(promptbox.Text) || promptbox.Text.Length < 2 || promptbox.Text.Equals("Paste or write here"))
 			{
@@ -39,15 +43,31 @@ namespace SimpleChatAPI
 			cleanded_prompt = System.Text.RegularExpressions.Regex.Replace(cleanded_prompt, @"\s+", " ");
 			return cleanded_prompt;
 		}
-
-		async Task curl()
+		
+		void taxes(dynamic jsonResponse)
 		{
-			if(!checkInputs())
-				return;
+			int prompt_tokens = jsonResponse.usage.prompt_tokens;
+			int completion_tokens = jsonResponse.usage.completion_tokens;
+			int total_tokens = jsonResponse.usage.total_tokens;
 
-			string prompt = cleanprompt(promptbox.Text);
-			string apiKey = apibox.Text;
+			int total_spent;
+			if(string.IsNullOrEmpty(total_spent_box.Text) || !int.TryParse(total_spent_box.Text, out total_spent))
+			{
+				total_spent = 0;
+			}
+			total_spent += total_tokens;
+			total_spent_box.Text = total_spent.ToString();
+			prompt_token_box.Text = prompt_tokens.ToString();
+			completion_token_box.Text = completion_tokens.ToString();
+			total_token_box.Text = total_tokens.ToString();
+			current_spent_box.Text = (0.000002 * total_tokens).ToString("0.000000") + "$";
+			total_m_spent_box.Text = (0.000002 * total_spent).ToString("0.000000") + "$";
+		}
 
+		
+		async Task <string> GetResponse(string prompt_text, string system_role)
+		{
+			string prompt = cleanprompt(prompt_text);
 			var httpClient = new HttpClient();
 			httpClient.Timeout = TimeSpan.FromSeconds(20);
 			httpClient.DefaultRequestHeaders.Accept.Clear();
@@ -81,27 +101,39 @@ namespace SimpleChatAPI
 			{
 				string errorMessage = "HTTP request failed: " + ex.Message;
 				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
+				return null;
 			}
-			
+
 			// Process API response
 			string responseContent;
 			if(response.IsSuccessStatusCode)
 			{
 				responseContent = await response.Content.ReadAsStringAsync();
+				taxes(JsonConvert.DeserializeObject(responseContent));
+				return responseContent;
 			}
 			else
 			{
 				responseContent = await response.Content.ReadAsStringAsync();
 				string errorMessage = $"API request failed with status code {response.StatusCode}. Reason: {response.ReasonPhrase}. Response content: {responseContent}";
 				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
+				return null;
 			}
+		}
+
+		async Task send_prompt()
+		{
+			if(!checkInputs())
+				return;
+
+			string response = await GetResponse(promptbox.Text, "-");
+			if(response == null)
+				return;
 
 			// Parse JSON response
 			try
 			{
-				dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
+				dynamic jsonResponse = JsonConvert.DeserializeObject(response);
 				if(checkResponse(jsonResponse))
 				{
 					string messageContent = jsonResponse.choices[0].message.content;
@@ -136,9 +168,9 @@ namespace SimpleChatAPI
 			return true;
 		}
 
-		private async void button1_Click(object sender, EventArgs e)
+		private async void send_prompt_click(object sender, EventArgs e)
 		{
-			await curl();
+			await send_prompt();
 		}
 
 		private void apibox_TextChanged(object sender, EventArgs e)
@@ -208,67 +240,23 @@ namespace SimpleChatAPI
 				return;
 
 			string prompt = cleanprompt(promptbox.Text);
-			string apiKey = apibox.Text;
 
-			// Configure HttpClient
-			var httpClient = new HttpClient();
-			httpClient.Timeout = TimeSpan.FromSeconds(20);
-			httpClient.DefaultRequestHeaders.Accept.Clear();
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			prompt = $"'{prompt}' - 10 distinct, concise prompts starting with '-' to aid understanding.";
+			string response = await GetResponse(prompt, "-");
+			if(response == null)
+				return;
 
-			// Prepare API request
 			//{ "role": "system", "content": "You're a smart prompt suggestion assistant."}
+			//content = $"Based on the text '{prompt}', please generate four comprehensive lists with descriptive titles, each consisting of five prompts that start with a hyphen (-). We aim to obtain three distinct categories, each containing five useful prompts related to the text provided. Please ensure that the prompts are informative, relevant, and helpful in understanding the given text."
+			//content = $"Provide me 4 thorough named lists of different topics with each list having 5 helpful prompts, each item starts with \"-\", based on the following text: {prompt}"
+			//Generate 3 lists of 5 informative prompts that start with a hyphen (-) based on the given text '{prompt}', organized into distinct categories for clarity.The prompts should be relevant and help in understanding the text.
+			//content = $"Generate 3 lists of 5 informative prompts that start with a hyphen (-) based on the given text '{prompt}', organized into distinct categories for clarity.The prompts should be relevant and help in understanding the text."
 
-			const string apiEndpoint = "https://api.openai.com/v1/chat/completions";
-			var requestBody = new
-			{
-				model = "gpt-3.5-turbo",
-				messages = new[]
-				{
-					new
-					{
-						role = "user",
-						content = $"Provide me a thorough list of helpful but smart prompts so, each item starts with \"-\", based on the following text: {prompt}"
-					}
-				}
-			};
-
-			var requestContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
-			var request = new HttpRequestMessage(HttpMethod.Post, apiEndpoint);
-			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
-			request.Content = requestContent;
-
-			// Send API request
-			HttpResponseMessage response;
-			try
-			{
-				response = await httpClient.SendAsync(request);
-			}
-			catch(HttpRequestException ex)
-			{
-				string errorMessage = "HTTP request failed: " + ex.Message;
-				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
-
-			// Process API response
-			string responseContent;
-			if(response.IsSuccessStatusCode)
-			{
-				responseContent = await response.Content.ReadAsStringAsync();
-			}
-			else
-			{
-				responseContent = await response.Content.ReadAsStringAsync();
-				string errorMessage = $"API request failed with status code {response.StatusCode}. Reason: {response.ReasonPhrase}. Response content: {responseContent}";
-				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
 
 			// Parse JSON response
 			try
 			{
-				dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
+				dynamic jsonResponse = JsonConvert.DeserializeObject(response);
 
 				if(checkResponse(jsonResponse))
 				{
@@ -312,77 +300,55 @@ namespace SimpleChatAPI
 			await runthis();
 		}
 
-		async Task runthis()
+		async Task dothis(string cmd)
 		{
 			if(!checkInputs())
 				return;
-
 			string prompt = cleanprompt(promptbox.Text);
-			string apiKey = apibox.Text;
+			string content = $"{cmd} : {prompt}";
 
-			// Configure HttpClient
-			var httpClient = new HttpClient();
-			httpClient.Timeout = TimeSpan.FromSeconds(20);
-			httpClient.DefaultRequestHeaders.Accept.Clear();
-			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-			// Prepare API request
-			const string apiEndpoint = "https://api.openai.com/v1/chat/completions";
-			// here we are going to append to the string cmd the value of the selected mysterylistbox item
-			string cmd = mysterylistbox.SelectedItem.ToString();
-			var requestBody = new
-			{
-				model = "gpt-3.5-turbo",
-				messages = new[]
-				{
-					new
-					{
-						role = "user",
-						content = $"{cmd} based on the following text: {prompt}"
-					}
-				}
-			};
-
-			var requestContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
-			var request = new HttpRequestMessage(HttpMethod.Post, apiEndpoint);
-			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
-			request.Content = requestContent;
-
-			// Send API request
-			HttpResponseMessage response;
-			try
-			{
-				response = await httpClient.SendAsync(request);
-			}
-			catch(HttpRequestException ex)
-			{
-				string errorMessage = "HTTP request failed: " + ex.Message;
-				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			string response = await GetResponse(content, "");
+			if(response == null)
 				return;
-			}
-
-			// Process API response
-			string responseContent;
-			if(response.IsSuccessStatusCode)
-			{
-				responseContent = await response.Content.ReadAsStringAsync();
-			}
-			else
-			{
-				responseContent = await response.Content.ReadAsStringAsync();
-				string errorMessage = $"API request failed with status code {response.StatusCode}. Reason: {response.ReasonPhrase}. Response content: {responseContent}";
-				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
 
 			// Parse JSON response
 			try
 			{
-				dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
+				dynamic jsonResponse = JsonConvert.DeserializeObject(response);
 				if(checkResponse(jsonResponse))
 				{
 					string messageContent = jsonResponse.choices[0].message.content;
-					screenbox.Text = messageContent;
+					resultbox.Text = messageContent;
+				}
+			}
+			catch(JsonException ex)
+			{
+				string errorMessage = "Failed to parse JSON response: " + ex.Message;
+				MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+		}
+
+		async Task runthis()
+		{
+			if(!checkInputs())
+				return;
+			string prompt = cleanprompt(promptbox.Text);
+			string cmd = mysterylistbox.SelectedItem.ToString();
+			string content = $"{cmd} based on the following text: {prompt}";
+
+			string response = await GetResponse(content, "");
+			if(response == null)
+				return;
+
+			// Parse JSON response
+			try
+			{
+				dynamic jsonResponse = JsonConvert.DeserializeObject(response);
+				if(checkResponse(jsonResponse))
+				{
+					string messageContent = jsonResponse.choices[0].message.content;
+					resultbox.Text = messageContent;
 				}
 			}
 			catch(JsonException ex)
@@ -396,6 +362,12 @@ namespace SimpleChatAPI
 		private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
 			System.Diagnostics.Process.Start("https://www.buymeacoffee.com/dimitrismitsos");
+		}
+
+		private async void button1_Click_1(object sender, EventArgs e)
+		{
+			string cmd = comboBox1.SelectedItem.ToString();
+			await dothis(cmd);
 		}
 	}
 }
